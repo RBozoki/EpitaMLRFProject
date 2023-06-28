6730f607f9f7a63067ca95d1744388f115f9f5d9
Raphael Bozoki <raphaelbozoki01@gmail.com>
2023-06-28 10:01:09 +0200

    add a hog_descriptor column to the dataframe

--- src/features/hog_features.py
+++ src/features/hog_features.py
@@ -37,3 +37,31 @@ plt.imshow(hog_image_rescaled, cmap=plt.cm.gray)
 plt.show()
 
 #%%
+
+from tqdm import tqdm
+
+# Initialisez une liste vide pour stocker les descripteurs HOG
+hog_descriptors = []
+
+# Bouclez sur toutes les images du DataFrame
+for i in tqdm(range(df.shape[0])):
+    image_data = df.iloc[i]["data"]
+    image_data = ast.literal_eval(image_data)
+    image = np.reshape(image_data, (32, 32, 3), order='F')
+    image = np.transpose(image, (1, 0, 2))
+    image = image.astype('uint8')
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Calculez le descripteur HOG pour l'image
+    hog_descriptor = hog(image_gray, orientations=8, pixels_per_cell=(16, 16),
+                         cells_per_block=(1, 1), visualize=False)
+
+    # Ajoutez le descripteur HOG à la liste
+    hog_descriptors.append(hog_descriptor)
+
+# Ajoutez les descripteurs HOG en tant que nouvelle colonne dans le DataFrame
+df["hog_descriptor"] = hog_descriptors
+
+# Afficher les premières lignes du DataFrame pour vérifier
+print(df.head())
+#%%
