254919a154ba576fff6a47cf5ae35cb20d94c44d
Raphael Bozoki <raphaelbozoki01@gmail.com>
2023-06-28 10:11:53 +0200

    generate csv files with a hog_descriptor column

--- src/features/hog_features.py
+++ src/features/hog_features.py
@@ -1,3 +1,4 @@
+
 import matplotlib
 import pandas as pd
 import numpy as np
@@ -64,4 +65,34 @@ df["hog_descriptor"] = hog_descriptors
 
 # Afficher les premières lignes du DataFrame pour vérifier
 print(df.head())
+#%%
+import glob
+
+data_files = glob.glob("../../data/processed/data_batch_*.csv")
+
+# Bouclez sur tous les fichiers trouvés
+for file_path in data_files:
+    df = pd.read_csv(file_path)
+
+    hog_descriptors = []
+    for i in tqdm(range(df.shape[0])):
+        image_data = df.iloc[i]["data"]
+        image_data = ast.literal_eval(image_data)
+        image = np.reshape(image_data, (32, 32, 3), order='F')
+        image = np.transpose(image, (1, 0, 2))
+        image = image.astype('uint8')
+        image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        hog_descriptor = hog(image_gray, orientations=8, pixels_per_cell=(16, 16),
+                             cells_per_block=(1, 1), visualize=False)
+        hog_descriptors.append(hog_descriptor)
+
+    df["hog_descriptor"] = hog_descriptors
+
+    # Créer un nouveau chemin de fichier pour sauvegarder les données prétraitées
+    new_file_path = file_path.replace("processed", "interim")
+
+    # Enregistrez le DataFrame dans le nouveau fichier
+    df.to_csv(new_file_path, index=False)
+
 #%%
