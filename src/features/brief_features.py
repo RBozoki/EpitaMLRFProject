3e54e6409d599e91745dde5d0473dfc828a585d7
Raphael Bozoki <raphaelbozoki01@gmail.com>
2023-07-05 12:54:02 +0200

    brief_features.py

---
+++ src/features/brief_features.py
@@ -0,0 +1,54 @@
+import glob
+import os
+import pandas as pd
+import ast
+import cv2
+import numpy as np
+from tqdm import tqdm
+from skimage import exposure
+
+# Initialisation du détecteur de points clés FAST et du descripteur BRIEF
+fast = cv2.FastFeatureDetector_create()
+brief = cv2.xfeatures2d.BriefDescriptorExtractor_create()
+
+data_files = glob.glob("data/processed/data_batch_*.csv")
+
+# Bouclez sur tous les fichiers trouvés
+for file_path in data_files:
+    df = pd.read_csv(file_path)
+
+    brief_descriptors = []
+    for i in tqdm(range(df.shape[0])):
+        image_data = df.iloc[i]["data"]
+        image_data = ast.literal_eval(image_data)
+        image = np.reshape(image_data, (32, 32, 3), order='F')
+        image = np.transpose(image, (1, 0, 2))
+        image = image.astype('uint8')
+        image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Detection de points clés avec FAST
+        keypoints = fast.detect(image_gray, None)
+
+        # Extraction de descripteurs BRIEF
+        keypoints, descriptor = brief.compute(image_gray, keypoints)
+
+        if descriptor is not None:
+            # Les descripteurs sont renvoyés comme un tableau 2D,
+            # nous devons donc les aplatir en un tableau 1D
+            descriptor = descriptor.flatten()
+        else:
+            # Si aucun point clé n'est détecté, le descripteur sera None,
+            # donc nous devons créer un descripteur de zéros
+            descriptor = np.zeros(brief.descriptorSize(), dtype=np.float32)
+
+        brief_descriptors.append(descriptor.tolist())
+
+    df["brief_descriptor"] = brief_descriptors
+
+    # Créer un nouveau chemin de fichier pour sauvegarder les données prétraitées
+    new_file_path = file_path.replace("processed", "interim/brief")
+
+    os.makedirs(os.path.dirname(new_file_path), exist_ok=True)
+
+    # Enregistrez le DataFrame dans le nouveau fichier
+    df.to_csv(new_file_path, index=False)
